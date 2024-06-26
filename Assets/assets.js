70b4ac8eaf3fa9ada36b9a844b64cd9cdf33facf
Riddhi <riddhinimeshshah10@gmail.com>
2024-06-26 10:59:36 +0530

    git setup complete

---
+++ Assets/assets.js
@@ -0,0 +1,193 @@
+import blog_pic_1 from './brazil.png';
+import blog_pic_2 from './spain.jpeg';
+import blog_pic_3 from './usa.jpeg';
+import facebook_icon from './fb.jpeg'
+import profile_icon from './profile.png'
+import instagram_icon from './ig.jpeg'
+import twitter_icon from './twitter.jpeg'
+import logo from './logo.png';
+import arrow from './arrow.png';
+import logo_light from './logo_dark.png'
+import blog_icon from './blog_icon.png'
+import add_icon from './add_icon.png'
+import email_icon from './email.png'
+import upload_area from './upload.jpeg'
+
+export const assets = {
+  facebook_icon,
+  instagram_icon,
+  twitter_icon,
+  profile_icon,
+  logo,
+  arrow,
+  logo_light,
+  blog_icon,
+  add_icon,
+  email_icon,
+  upload_area
+}
+
+export const blog_data = [{
+    id:1,
+    title: "10 LESSER KNOWN AND CAPTIVATING FACTS ABOUT BRAZIL",
+    description:"Brazil stands out globally for several reasons. It is the largest exporter of coffee, producing over 3 million tons annually, with its balanced and nutty Arabica beans highly favored worldwide.",
+    image:blog_pic_1,
+    date: Date.now(),
+    category: "Lifestyle",
+    author: "Alex Bennett",
+    author_img:profile_icon
+},
+{
+    id:2, 
+    title: "How to create an effective startup roadmap or ideas",
+    description: "Spain is rich in cultural and historical heritage, boasting 45 UNESCO World Heritage Sites, including the Alhambra and Gaudí’s masterpieces. Spanish, spoken by over 580 million people, is the official language in 21 countries. As the largest producer of olive oil, Spain accounts for about 40% of global production.",
+    image:blog_pic_2,
+    date: Date.now(),
+    category: "Startup", 
+    author: "Alex Bennett",
+    author_img:profile_icon
+}, 
+{ 
+    id:3,
+    title: "Learning new technology to boost your career in software",
+    description: "Lorem Ipsum is simply dummy text of the printing and typese",
+    image:blog_pic_3,
+    date: Date.now(),
+    category: "Technology",
+    author: "Alex Bennett",
+    author_img:profile_icon
+},
+{
+    id: 4,
+    title: "Tips for getting the most out of apps and software",
+    description: "Lorem Ipsum is simply dummy text of the printing and typeset\n",
+    image: blog_pic_1,
+    date: Date.now(),
+    category: "Technology",
+    author: "Alex Bennett",
+    author_img: profile_icon
+  },
+  {
+    id: 5,
+    title: "Enhancing your skills and capturing memorable moments",
+    description: "Lorem Ipsum is simply dummy text of the printing and typeset\n",
+    image: blog_pic_2,
+    date: Date.now(),
+    category: "Lifestyle",
+    author: "Alex Bennett",
+    author_img: profile_icon
+  },
+  {
+    id: 6,
+    title: "Maximizing returns by minimizing resources in your startup",
+    description: "Lorem Ipsum is simply dummy text of the printing and typeset\n",
+    image: blog_pic_3,
+    date: Date.now(),
+    category: "Startup",
+    author: "Alex Bennett",
+    author_img: profile_icon
+  },
+  {
+    id:7,
+    title: "10 LESSER KNOWN AND CAPTIVATING FACTS ABOUT BRAZIL",
+    description:"Brazil stands out globally for several reasons. It is the largest exporter of coffee, producing over 3 million tons annually, with its balanced and nutty Arabica beans highly favored worldwide. Geographically, Brazil borders nearly every South American country except Chile and Ecuador, highlighting its strategic position on the continent. The country is a biodiversity hotspot, home to at least 15% of the world's species, including over 100,000 invertebrates and 800 bird species. .",
+    image:blog_pic_1,
+    date: Date.now(),
+    category: "Lifestyle",
+    author: "Alex Bennett",
+    author_img:profile_icon
+},
+{
+    id:8, 
+    title: "How to create an effective startup roadmap or ideas",
+    description: "Spain is rich in cultural and historical heritage, boasting 45 UNESCO World Heritage Sites, including the Alhambra and Gaudí’s masterpieces. Spanish, spoken by over 580 million people, is the official language in 21 countries. As the largest producer of olive oil, Spain accounts for about 40% of global production. The Sagrada Familia’s construction began in 1882 and continues today. Spain's love for football is evident with La Liga teams like Real Madrid and Barcelona. The tradition of siesta is still cherished, and tapas originated here. Spain hosts La Tomatina, the world's largest food fight, and the running of the bulls at Pamplona's San Fermín festival. Flamenco music and dance originated in Andalusia. Spain has the world's largest number of Blue Flag beaches and the oldest restaurant, Sobrino de Botín, established in 1725. Lastly, Spain recognizes four official languages: Spanish, Catalan, Galician, and Basque.",
+    image:blog_pic_2,
+    date: Date.now(),
+    category: "Startup", 
+    author: "Alex Bennett",
+    author_img:profile_icon
+}, 
+{ 
+    id:9,
+    title: "Learning new technology to boost your career in software",
+    description: "Lorem Ipsum is simply dummy text of the printing and typese",
+    image:blog_pic_3,
+    date: Date.now(),
+    category: "Technology",
+    author: "Alex Bennett",
+    author_img:profile_icon
+},
+{
+    id: 10,
+    title: "Tips for getting the most out of apps and software",
+    description: "Lorem Ipsum is simply dummy text of the printing and typeset\n",
+    image: blog_pic_1,
+    date: Date.now(),
+    category: "Technology",
+    author: "Alex Bennett",
+    author_img: profile_icon
+  },
+  {
+    id: 11,
+    title: "Enhancing your skills and capturing memorable moments",
+    description: "Lorem Ipsum is simply dummy text of the printing and typeset\n",
+    image: blog_pic_2,
+    date: Date.now(),
+    category: "Lifestyle",
+    author: "Alex Bennett",
+    author_img: profile_icon
+  },
+  {
+    id: 12,
+    title: "Maximizing returns by minimizing resources in your startup",
+    description: "Lorem Ipsum is simply dummy text of the printing and typeset\n",
+    image: blog_pic_3,
+    date: Date.now(),
+    category: "Startup",
+    author: "Alex Bennett",
+    author_img: profile_icon
+  },
+  { 
+    id:13,
+    title: "Learning new technology to boost your career in software",
+    description: "Lorem Ipsum is simply dummy text of the printing and typese",
+    image:blog_pic_3,
+    date: Date.now(),
+    category: "Technology",
+    author: "Alex Bennett",
+    author_img:profile_icon
+},
+{
+    id: 14,
+    title: "Tips for getting the most out of apps and software",
+    description: "Lorem Ipsum is simply dummy text of the printing and typeset\n",
+    image: blog_pic_1,
+    date: Date.now(),
+    category: "Technology",
+    author: "Alex Bennett",
+    author_img: profile_icon
+  },
+  {
+    id: 15,
+    title: "Enhancing your skills and capturing memorable moments",
+    description: "Lorem Ipsum is simply dummy text of the printing and typeset\n",
+    image: blog_pic_2,
+    date: Date.now(),
+    category: "Lifestyle",
+    author: "Alex Bennett",
+    author_img: profile_icon
+  },
+  {
+    id: 16,
+    title: "Maximizing returns by minimizing resources in your startup",
+    description: "Lorem Ipsum is simply dummy text of the printing and typeset\n",
+    image: blog_pic_3,
+    date: Date.now(),
+    category: "Startup",
+    author: "Alex Bennett",
+    author_img: profile_icon
+  }
+
+  
+
+];
---
+++ app/page.js
@@ -0,0 +1,11 @@
+'use client'
+import Header from '../Components/Header';
+import BlogList from "@/Components/BlogList";
+export default function Home() {
+  return (
+    <>
+      <Header/>
+      <BlogList/>
+    </>
+  );
+}
