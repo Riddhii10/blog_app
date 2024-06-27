e16d3b6cc7f7d74ae5a144c1d2918cb4a79dda98
Riddhi <riddhinimeshshah10@gmail.com>
2024-06-27 13:42:43 +0530

    delete functionality added

--- app/admin/subscriptions/page.jsx
+++ app/admin/subscriptions/page.jsx
@@ -1,9 +1,53 @@
 'use client'
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import SubsTableItem from '@/Components/AdminComponents/SubsTableItem'
+import axios from 'axios';
+import { toast } from 'react-toastify';
 const page = () => {
+  const [emails,setEmails]=useState([]);
+  const fetchEmails=async()=>{
+    const response=await axios.get('/api/email');
+    setEmails(response.data.emails);
+  }
+  const deleteEmail=async(mongoId)=>{
+    const response=await axios.delete("/api/email",{
+      params:{
+        id:mongoId
+      }
+    })
+    if(response.data.success){
+      toast.success(response.data.msg)
+      fetchEmails();
+    }
+    else{
+      toast.error("Cant cancel subscription ")
+    }
+  }
+  useEffect(()=>{
+    fetchEmails();
+  },[]);
+
   return (
-    <div></div>
+    <div className='flex-1 pt-5 px-5 sm:pt-12 pl-16'>
+      <h1>All Subcription</h1>
+      <div className='relative max-w-[600px] h-[80vh] overflow-x-auto mt-4 border border-gray-400 scrollbar-hide'>
+        <table className='w-full text-sm text-gray-500'>
+          <thead className='text-xs text-left text-gray-700 uppercase bg-gray-50'>
+            <tr>
+              <th scope='col' className='px-6 py-3'>Email Subscription</th>
+              <th scope='col' className='px-6 py-3 hidden sm:block'>Date</th>
+              <th scope='col' className='px-6 py-3'>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {emails.map((item,index)=>{
+                return <SubsTableItem key={index} mongoId={item._id} email={item.email} date={item.date} deleteEmail={deleteEmail}/>
+            })}
+            
+          </tbody>
+        </table>
+      </div>
+    </div>
   )
 }
 
