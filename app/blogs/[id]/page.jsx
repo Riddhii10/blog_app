f907f7086103b1aae086b0988f2747b9046c45f7
Riddhi <riddhinimeshshah10@gmail.com>
2024-06-27 13:53:07 +0530

    final touches

--- app/blogs/[id]/page.jsx
+++ app/blogs/[id]/page.jsx
@@ -43,20 +43,11 @@ const page = ({params}) => {
     </div>
     <div className='mx-5 max-w-[800px] md:mx-auto mt-[-100px] mb-10'>
         <Image src={data.image} width={1280} alt='' height={250} className='border-4 border-white mx-auto'/>
-        <h1 className='my-8 text-[26px] font-semibold'>Introduction</h1>
-        <p>{data.description}</p>
-        <h3 className='my-5 text-[18px] font-semibold'>GOAL 1 </h3>
-        <p className='my-3'>Yeah man, that was good. Let's do another one. Lorraine, have you ever, uh, been in a situation where you know you had to act a certain way but when you got there, you didn't know if you could go through with it? Wrecked? When did this happen and- Your not gonna be picking a fight, Dad, dad dad daddy-o. You're coming to a rescue, right? Okay, let's go over the plan again. 8:55, where are you gonna be. Nothing's coming to my mind.</p>
-
-        <h3 className='my-5 text-[18px] font-semibold'>GOAL 2 </h3>
-        <p className='my-3'>Get your meat hooks off of me. Oh, oh a rematch, why, were you cheating? I don't wanna see you in here again. Its good. Doc? Am I to understand you're still hanging around with Doctor Emmett Brown, McFly? Tardy slip for you, Miss Parker. And one for you McFly I believe that makes four in a row. Now let me give you a nickle's worth of advice, young man. This so called Doctor Brown is dangerous, he's a real nutcase. You hang around with him you're gonna end up in big trouble.</p>
-    
-        <h3 className='my-5 text-[18px] font-semibold'>GOAL 3 </h3>
-        <p className='my-3'>I swiped it from the old lady's liquor cabinet. Biff. Well yeah, you know we have two of them. Well, uh, listen, uh, I really- Well, because George, nice girls get angry when guys take advantage of them.</p>
-
-        <h3 className='my-5 text-[18px] font-semibold'>Conclusion </h3>
-        <p className='my-3'>Marty, you interacted with anybody else today, besides me? Marty, you made it. Well looky what we have here. No no no, you're staying right here with me. Because, you might regret it later in life. Okay, okay you guys, oh ha ha ha very funny. Hey you guys are being real mature</p>
-
+        {/* <h1 className='my-8 text-[26px] font-semibold'>Introduction</h1> */}
+        {/* <p>{data.description}</p> */}
+        <div className='blog-content' dangerouslySetInnerHTML={{__html:data.description}}>
+          
+        </div>
         <div className='my-24'>
           <p className='text-black font-semibold my-4 text-lg'>shareKaro.</p>
           <div className='flex gap-3 '>
