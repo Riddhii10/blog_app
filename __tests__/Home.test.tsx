29b063fff45a5a189dfa5ae9e5cf9d4daa5a0bf2
Riddhi <riddhinimeshshah10@gmail.com>
2024-07-02 13:35:48 +0530

    .

--- __tests__/Home.test.tsx
+++ __tests__/Home.test.tsx
@@ -1,10 +1,20 @@
 import {render,screen} from '@testing-library/react'
 import Home from '@/app/page'
 
-it('should have dark text',()=>{
-    render(<Home/>)   //ARRANGE
+// it('should have dark text',()=>{
+//     render(<Home/>)   //ARRANGE
 
-    const myElem=screen.getByText('dark')      //ACT
+//     const myElem=screen.getByText('dark')      //ACT
 
-    expect(myElem).toBeInTheDocument()  //ASSERT 
-})
+//     expect(myElem).toBeInTheDocument()  //ASSERT 
+// })
+
+// __tests__/Home.test.js
+describe('Home', () => {
+  it('renders the ToastContainer', () => {
+    render(<Home />);
+    const toastContainer = screen.getByRole('alert');
+    expect(toastContainer).toBeInTheDocument();
+  });
+
+});
