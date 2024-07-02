63cce69b9beb3d43b69aa170f97f0c526741176b
Riddhi <riddhinimeshshah10@gmail.com>
2024-07-02 17:51:30 +0530

    6 test cases

--- __tests__/Header.test.js
+++ __tests__/Header.test.js
@@ -1,18 +1,18 @@
 import React from 'react';
-
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import Header from '@/Components/Header';
-
-import axios from 'axios'; // Import Axios
+import axios from 'axios';
 import { toast } from 'react-toastify';
 
 // Mock Axios post function
 jest.mock('axios');
+
+// Mock toast functions
 jest.mock('react-toastify', () => ({
   toast: {
     success: jest.fn(),
-    error: jest.fn(),
-  },
+    error: jest.fn()
+  }
 }));
 
 describe('Header Component', () => {
@@ -37,42 +37,41 @@ describe('Header Component', () => {
     fireEvent.change(emailInput, { target: { value: 'test@example.com' } });
     expect(emailInput.value).toBe('test@example.com');
   });
-  
+
   it('renders a button with the correct text', () => {
     const { container } = render(<Header />);
     const button = container.querySelector('button');
     expect(button).toHaveTextContent('Get started');
   });
-  // it('calls axios post and shows success toast on successful form submission', async () => {
-  //   axios.post.mockResolvedValue({ data: { success: true, msg: 'Subscribed successfully!' } });
-
-  //   render(<Header />);
-
-  //   const emailInput = screen.getByPlaceholderText('Enter the email.');
-  //   fireEvent.change(emailInput, { target: { value: 'test@example.com' } });
-
-  //   const submitButton = screen.getByText('Subscribe');
-  //   fireEvent.click(submitButton);
-
-  //   await waitFor(() => expect(axios.post).toHaveBeenCalledWith('/api/email', expect.any(FormData)));
-  //   await waitFor(() => expect(toast.success).toHaveBeenCalledWith('Subscribed successfully!'));
-  //   expect(emailInput.value).toBe('');
-  // });
-
-  // it('calls axios post and shows error toast on failed form submission', async () => {
-  //   axios.post.mockResolvedValue({ data: { success: false } });
-
-  //   render(<Header />);
-
-  //   const emailInput = screen.getByPlaceholderText('Enter the email.');
-  //   fireEvent.change(emailInput, { target: { value: 'test@example.com' } });
-
-  //   const submitButton = screen.getByText('Subscribe');
-  //   fireEvent.click(submitButton);
-
-  //   await waitFor(() => expect(axios.post).toHaveBeenCalledWith('/api/email', expect.any(FormData)));
-  //   await waitFor(() => expect(toast.error).toHaveBeenCalledWith('Error...'));
-  // });
-
   
+  it('calls axios post and shows success toast on successful form submission', async () => {
+    axios.post.mockResolvedValue({ data: { success: true, msg: 'Subscribed successfully!' } });
+
+    render(<Header />);
+
+    const emailInput = screen.getByPlaceholderText('Enter the email.');
+    fireEvent.change(emailInput, { target: { value: 'test@example.com' } });
+
+    const submitButton = screen.getByText('Subscribe');
+    fireEvent.click(submitButton);
+
+    await waitFor(() => expect(axios.post).toHaveBeenCalledWith('/api/email', expect.any(FormData)));
+    await waitFor(() => expect(toast.success).toHaveBeenCalledWith('Subscribed successfully!'));
+    expect(emailInput.value).toBe('');
+  });
+
+  it('calls axios post and shows error toast on failed form submission', async () => {
+    axios.post.mockResolvedValue({ data: { success: false } });
+
+    render(<Header />);
+
+    const emailInput = screen.getByPlaceholderText('Enter the email.');
+    fireEvent.change(emailInput, { target: { value: 'test@example.com' } });
+
+    const submitButton = screen.getByText('Subscribe');
+    fireEvent.click(submitButton);
+
+    await waitFor(() => expect(axios.post).toHaveBeenCalledWith('/api/email', expect.any(FormData)));
+    await waitFor(() => expect(toast.error).toHaveBeenCalledWith('Error...'));
+  });
 });
